1b1025bd94cb278f55e7b0dd6c78d01c69152f14
0Aniket Mishra <0aniketmishra@gmail.com>
2024-09-05 10:00:17 +0530

    Update.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const nextConfig = {
     images: {
         domains: ['picsum.photos'], // Add your image domains here
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://identitytoolkit.googleapis.com/v1/:path*',
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                ],
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
