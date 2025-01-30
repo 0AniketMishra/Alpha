4e162763ea327027ed3cd7acdbd81a389563b529
0Aniket Mishra <0aniketmishra@gmail.com>
2025-01-30 21:00:36 +0530

    Similar Updates.

--- app/components/Grid.js
+++ app/components/Grid.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image';
 import ProductCard from './ProductCard';
+import ProductCardSkeleton from './ProductCardSkeleton';
 
 const Grid = () => {
     const dummy = "This is dummy Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa eget egestas purus viverra accumsan in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Turpis massa sed elementum tempus egestas sed sed risus pretium. Dignissim enim sit amet venenatis urna cursus. Habitant morbi tristique senectus et netus et malesuada fames. Viverra adipiscing at in tellus integer feugiat. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet."
 
-    const [listings, setListings] = useState([]);
+    const [listings, setListings] = useState([{_id: 1},{_id: 2},{_id: 3},{_id: 4},{_id: 5}, {_id: 6}, {_id: 7}, {_id: 8}]);
+    const [loading, setLoading] = useState(true)
 
      useEffect(() => {
          const fetchListings = async () => {
@@ -14,6 +16,7 @@ const Grid = () => {
              const response = await fetch("https://alpha-backend-v7bb.vercel.app/listings");
               const data = await response.json();
                setListings(data);
+               setLoading(false)
              } catch (error) {
                  console.error('Error fetching listings:', error);
                  } }; 
@@ -28,7 +31,7 @@ const Grid = () => {
                     <div className="flex flex-wrap -m-2" >                                    
                             {listings.map((product) => (
                                 <div key={product._id} className='2xl:w-1/4 xl:w-1/3 lg:w-1/3  rounded-lg md:w-1/2 p-2 w-full '>
-                                    <ProductCard {...product} />
+                                   {loading ? <ProductCardSkeleton/> : <ProductCard {...product}/>}
                                 </div>
                             ))}
                     </div>
--- app/components/ProductCardSkeleton.js
+++ app/components/ProductCardSkeleton.js
@@ -6,31 +6,31 @@ function ProductCardSkeleton() {
       
         <div className="aspect-16/10 w-full overflow-hidden rounded-t-xl bg-gray-200">
           <div
-            className="h-full w-full object-cover object-center group-hover:opacity-75 transition-opacity bg-gray-600 "
+            className="h-full w-full object-cover object-center group-hover:opacity-75 transition-opacity bg-gray-300 "
           />
           <div className="absolute top-2 right-2 p-2 rounded-full bg-white/80 hover:bg-white dark:bg-gray-800/80 dark:hover:bg-gray-800"/>
         </div>
         <div className="mt-4 p-4">
           <div className="space-y-2 animate-pulse">
-            <div className="text-lg font-medium h-5 rounded-full dark:bg-gray-600 w-full line-clamp-1"></div>
-          <div className="text-lg font-medium h-5 rounded-full dark:bg-gray-600 w-full line-clamp-1"></div>
+            <div className="text-lg font-medium h-5 rounded-full dark:bg-gray-600 bg-gray-300 w-full line-clamp-1"></div>
+          <div className="text-lg font-medium h-5 rounded-full dark:bg-gray-600 bg-gray-300 w-full line-clamp-1"></div>
           </div>
-          <div className="mt-2  h-3 w-[80%] mt-4 rounded-full dark:bg-gray-600 animate-pulse"></div>
-        <div className="mt-2  h-3 w-[80%] mt-4 rounded-full dark:bg-gray-600 animate-pulse"></div>
+          <div className="mt-2  h-3 w-[80%] mt-4 rounded-full dark:bg-gray-600 bg-gray-300 animate-pulse"></div>
+        <div className="mt-2  h-3 w-[80%] mt-4 rounded-full dark:bg-gray-600 bg-gray-300 animate-pulse"></div>
 
           <div className="flex mt-2 items-center space-x-2">
             
-              <div className="text-md rounded-full dark:bg-gray-600 h-4 w-16"></div>
-          <div className="text-md rounded-full dark:bg-gray-600 h-4 w-16 animate-pulse"></div>
+              <div className="text-md rounded-full dark:bg-gray-600 bg-gray-300 h-4 w-16"></div>
+          <div className="text-md rounded-full dark:bg-gray-600 bg-gray-300 h-4 w-16 animate-pulse"></div>
             
           </div>
         </div>
       
       <div className="pl-4 pr-4 pb-4 space-y-3">
-        <button className="w-full bg-gray-100 text-gray-800 py-2 rounded-xl hover:bg-gray-200 transition-colors dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600">
+        <button className="w-full bg-gray-100 text-gray-800 py-2 rounded-xl  transition-colors dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600">
           Buy Now
         </button>
-        <button className="w-full dark:bg-orange-600  py-2 rounded-xl transition-colors ">
+        <button className="w-full bg-orange-600 text-white text-black  py-2 rounded-xl transition-colors ">
           Add to Cart
         </button>
       </div>
